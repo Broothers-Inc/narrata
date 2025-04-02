a621d9a0e3a2828b88f4dc16cea0cb5b22f91226
v i v a a n <vivaan.sinha2010@gmail.com>
2025-04-02 12:53:58 +0700

    RPG: Modified Inventory Test

--- tests/inventory.py
+++ tests/inventory.py
@@ -9,15 +9,17 @@ player_inventory.new_category("potions", "healing")
 player_inventory.new_category("potions", "damaging")
 
 # Adding items
-player_inventory.add_item("katana", {"damage": 12, "speed": 8}, "weapons", "melee", "swords")
+player_inventory.add_item("katana", {"damage": 12, "speed": 8}, "weapons", "melee", "blades")
 player_inventory.add_item("longbow", {"damage": 15, "range": 20}, "weapons", "ranged")
-player_inventory.add_item("broadsword", {"damage": 20, "speed": 4}, "weapons", "melee", "swords")
+player_inventory.add_item("broadsword", {"damage": 20, "speed": 4}, "weapons", "melee", "blades")
 player_inventory.add_item("bow", {"damage": 10, "range": 15}, "weapons", "ranged")
-player_inventory.add_item("spear", {"damage": 8, "speed": 6}, "weapons", "melee", "swords") 
+player_inventory.add_item("spear", {"damage": 8, "speed": 6}, "weapons", "melee", "blades") 
 player_inventory.add_item("healing potion", {"healing": 10}, "potions", "healing")
 player_inventory.add_item("damage potion", {"damage": 5}, "potions", "damaging")
 player_inventory.add_item("poison potion", {"poison": 3, "damage": 2}, "potions", "damaging")
+player_inventory.add_item("bat", {"damage": 10, "speed": 7}, "weapons", "melee", "other")
+player_inventory.add_item("mace", {"damage": 15, "speed": 4}, "weapons", "melee", "other")
 
 # Removing an item
-player_inventory.remove_item("katana", "weapons", "melee", "swords")
+player_inventory.remove_item("katana", "weapons", "melee", "blades")
 player_inventory.remove_item("longbow", "weapons", "ranged")
